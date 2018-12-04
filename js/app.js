8475036d22f96eee7689e6f7ed82df45d2cf3cc5
John Ciccone <cicconej93@gmail.com>
2018-12-04 13:21:15 -0500

    Update class syntax to ES6. Adjust enemy speed logic.

--- js/app.js
+++ js/app.js
@@ -1,85 +1,84 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+class Enemy {
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-
-    this.x = 1;
-    this.y = 125;
-
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    if(this.x > 500) {
-        this.x = -100;
-    } else this.x = this.x + 200 * dt;
-    //this.y = this.y + 1 * dt;
-    //console.log(dt);
-
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-var Player = function() {
-    this.sprite = 'images/char-boy.png';
-
-    this.x = 300;
-    this.y = 400;
-
-};
-
-Player.prototype.update = function(dt){
-    //console.log(dt);
-};
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
-Player.prototype.handleInput = function(input){
-    console.log(input);
-    switch (input) {
-        case 'left':
-            if (this.x > 25)
-                this.x = this.x - 50;
-            break;
-        case 'right':
-            if (this.x < 375)
-                this.x = this.x + 50;
-            break;
-        case 'up':
-            if (this.y > 25)
-                this.y = this.y - 50;
-            break;
-        case 'down':
-            if (this.y < 375)
-                this.y = this.y + 50;
-            break;
-        default:
-            console.log(wat);
+    constructor(x, y, speed){
+        this.sprite = 'images/enemy-bug.png'
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
 
     }
-};
 
+    update(dt){
+        // You should multiply any movement by the dt parameter
+        // which will ensure the game runs at the same speed for
+        // all computers.
+        if(this.x > 500) {
+            this.x = -100;
+            this.speed = Math.floor(Math.random() * 400) + 100;
+        } else this.x = this.x + this.speed * dt;
+    }
+
+    render(){
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+}
+
+//our player class
+class Player {
+
+    constructor(){
+        //introduce enum to randomly select player sprite?
+        this.sprite = 'images/char-boy.png';
+
+        this.x = 300;
+        this.y = 400;
+    }
+
+    //is this necessary?
+    update(dt) {
+
+    }
+
+    //draws the sprite to the screen at the specified location
+    render(){
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+    //takes user input
+    //might change this later, but works OK as is.
+    handleInput(input){
+
+        switch (input) {
+            case 'left':
+                if (this.x > 25)
+                    this.x = this.x - 50;
+                break;
+            case 'right':
+                if (this.x < 375)
+                    this.x = this.x + 50;
+                break;
+            case 'up':
+                if (this.y > 25)
+                    this.y = this.y - 50;
+                break;
+            case 'down':
+                if (this.y < 375)
+                    this.y = this.y + 50;
+                break;
+            default:
+                console.log(wat);
+        }
+    }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var newEnemy1 = new Enemy();
-var newEnemy2 = new Enemy();
-var newEnemy3 = new Enemy();
+var newEnemy1 = new Enemy(1, 143.5, 200);
+var newEnemy2 = new Enemy(1, 60, 350);
+var newEnemy3 = new Enemy(1, 225, 100);
 
 allEnemies = [newEnemy1, newEnemy2, newEnemy3];
 var player = new Player();
