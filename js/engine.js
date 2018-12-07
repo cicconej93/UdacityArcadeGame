f68c0ebd31d6cce579e0ca889a25f813939b4f1f
John Ciccone <cicconej93@gmail.com>
2018-12-06 22:35:55 -0500

    add update function call for game object

--- js/engine.js
+++ js/engine.js
@@ -94,6 +94,7 @@ var Engine = (function(global) {
             enemy.update(dt);
         });
         player.update();
+        game.update();
     }
 
     /* This function initially draws the "game level", it will then call
